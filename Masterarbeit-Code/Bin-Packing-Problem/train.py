f35b973b6ee353260f29c36f7defb5b609f47c69
Martin <martin.markert0@gmail.com>
2025-12-03 22:15:09 +0100

    Small improvementd and print statements for debugging/testing

--- Masterarbeit-Code/Bin-Packing-Problem/train.py
+++ Masterarbeit-Code/Bin-Packing-Problem/train.py
@@ -17,11 +17,11 @@ def train_and_evaluate(params, action_queue_list, result_queue_list):
     params.initialise()
 
 # Agent stuff
-    save_dir = params.save_dir                                                                  # Folder for checkpoints
+    save_dir = params.save_dir                                                                  # Folder for TensorBoard checkpoints
     writer = SummaryWriter("runs/" + save_dir)
     set_logging(save_dir)
 
-    logging.info(f"{'Step':>12}{'maxGoal':>12}{'expReturn':>12}{'expGoal':>12}{'CriticLoss':>12}{'ActorLoss':>12}{'LogProb':>12}")
+    logging.info(f"{'Step':>12} {'maxGoal':>12} {'expReturn':>12} {'expGoal':>12} {'CriticLoss':>12} {'ActorLoss':>12} {'LogProb':>12}")
     
     agent = Agent(bin_size_x            = params.bin_size_ds_x,
                   bin_size_y            = params.bin_size_ds_y,
@@ -42,16 +42,17 @@ def train_and_evaluate(params, action_queue_list, result_queue_list):
         return steps, average_reward
     
 # Start training
-    discount_factor       = params.discount_factor
-    break_step            = params.break_step
-    batch_size            = params.batch_size
-    target_step           = params.target_step
-    reward_scale          = params.reward_scale
-    repeat_times          = params.repeat_times
-    evaluator.total_step += params.load_step
+    discount_factor        = params.discount_factor
+    break_step             = params.break_step
+    batch_size             = params.batch_size
+    target_step            = params.target_step
+    reward_scale           = params.reward_scale
+    repeat_times           = params.repeat_times
+    evaluator.total_steps += params.load_step
     del params                                                                                  # memory clean-up
 
-    while evaluator.total_step < break_step:
+    while evaluator.total_steps < break_step:
+        print(f"{evaluator.total_steps} out of {break_step}")
         start_time_explore_environment = time.time()
         with torch.no_grad():
             trajectory_list = agent.explore_environment_multiprocessing(action_queue_list,
@@ -61,17 +62,18 @@ def train_and_evaluate(params, action_queue_list, result_queue_list):
                                                                         discount_factor
                                                                     )
             steps, average_reward = update_buffer(trajectory_list)
-            evaluator.total_step += steps
+            evaluator.total_steps += steps
             evaluator.save_model_if_it_is_better(agent.actor, agent.critic, agent.use_ratio_avg)
 
-        print(f"Time used for exploring the environment: {int(time.time() - start_time_explore_environment)} seconds")
+        print(f"Time used for exploring the environment: {int(time.time() - start_time_explore_environment)} second{'s' if int(time.time() - start_time_explore_environment) != 1 else ''}")
 
         start_time_update_net = time.time()
         logging_tuple = agent.update_net(buffer, batch_size, repeat_times)
 
-        print(f"Time used for updating actor and critic (update_net): {int(time.time() - start_time_update_net)} seconds")
+        print(f"Time used for updating actor and critic (update_net): {int(time.time() - start_time_update_net)} second{'s' if int(time.time() - start_time_update_net) != 1 else ''}")
 
         evaluator.tensorboard_writer(average_reward, logging_tuple, agent.use_ratio_avg, writer)# TensorBoard & Logging
+    print("done")
 
 
 class Evaluator:                                                                                # Used to save the best model during training and record training metrics for TensorBoard and log files.
@@ -80,29 +82,28 @@ class Evaluator:                                                                
                 ):
                 
         self.max_goal = -np.inf
-        self.total_step = 0
+        self.total_steps = 0
         self.cwd = cwd
-
-        logging.info(f"{'#' * 84}")
-        logging.info(f"{'Step':>12}{'maxGoal':>12}{'expReturn':>12}{'expGoal':>12}{'CriticLoss':>12}{'ActorLoss':>12}{'LogProb':>12}")
-        
+     
 
     def save_model_if_it_is_better(self, actor, critic, expected_goal):                         # Saves if the current performance is better:
         if expected_goal > self.max_goal:
             self.max_goal = expected_goal
             torch.save(actor.state_dict(), f'{self.cwd}/actor.pth')                             # Save the policy network in *.pth
             torch.save(critic.state_dict(), f'{self.cwd}/critic.pth')
-            logging.info(f"{self.total_step:12.4e}{self.max_goal:12.4f}")                     # Save the policy and print
+            logging.info(f"{self.total_steps:12} {self.max_goal:12.2f}")                        # Save the policy and print avlues. ONLY if the model is better than the previously best one
 
 
     def tensorboard_writer(self, expected_return, log_tuple, expected_goal, writer):            # Writes training metrics to TensorBoard and the log:
-        writer.add_scalar("expected_goal", expected_goal, self.total_step)                      # Logging values
-        writer.add_scalar("critic_loss", log_tuple[0], self.total_step)
-        writer.add_scalar("actor_loss", log_tuple[1], self.total_step)
-        writer.add_scalar("log_prob", log_tuple[2], self.total_step)
-        logging.info(f"{self.total_step:12.2e}{self.max_goal:12.4f}{expected_return:12.4f}{expected_goal:12.4f}{''.join(f'{n:12.4f}' for n in log_tuple)}")
+        writer.add_scalar("Expected_goal", expected_goal, self.total_steps)                     # Logging values
+        writer.add_scalar("Critic_loss", log_tuple[0], self.total_steps)
+        writer.add_scalar("Actor_loss", log_tuple[1], self.total_steps)
+        writer.add_scalar("Log_of_prob", log_tuple[2], self.total_steps)                        # p = 𝜋_𝜃(𝑎ₜ|𝑠ₜ) --> log(p)
+        
+        logging.info(f"{self.total_steps:12} {self.max_goal:12.2f} {expected_return:12.2f} {expected_goal:12.2f} {''.join(f'{n:12.2f}' for n in log_tuple)}")    # Full log of training
+        
         with open(self.cwd + "last_step.txt", "w") as f:                                        # File, which contains the last step. Purpose: training can be continued later from the last step without starting from 0.
-            f.write("{}".format(self.total_step))
+            f.write("{}".format(self.total_steps))
 
 
 # Helper functions
